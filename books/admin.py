f88c73f20d05a434e1d223b0ecbbd4471878bd7e
Julio Bastidas <juliobas@gmail.com>
2024-07-09 19:18:33 -0400

    agregando los modelos de me interesa, no me interesa, leidos y calificacion, desarrollado endpoint para obtener datos de perfil y actualizacion de perfil

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, BookRead, BookLike, BookDislike, BookRating
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(BookRead)
+admin.site.register(BookLike)
+admin.site.register(BookDislike)
+admin.site.register(BookRating)
