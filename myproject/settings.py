b59cc25574d499c373cd843ab6c642662be45b41
emaenriquez <emaenriquez1985@gmail.com>
2024-07-17 20:24:53 -0300

    modificacion en el archivo de configuracion

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'books',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -131,3 +133,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+# Configura los orígenes permitidos
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Cambia esto si tu frontend se ejecuta en otro puerto
+]
